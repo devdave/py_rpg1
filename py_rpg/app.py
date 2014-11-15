0b6740041a166a98571dd50916b4e8b7b26edddf
David W <devdave@ominian.net>
2014-11-14 21:09:22 -0700

    ABANDON THREAD
    I like part of where this code is going but I've also made too many
    little mistakes.  Committing what I've done and spawning off a new
    branch to try a different approach.

--- py_rpg/app.py
+++ py_rpg/app.py
@@ -10,7 +10,6 @@ from flask import Flask
 # being assigned to make it available to the rest of the app.
 from flask import g
 
-from flask import request
 from flask import session
 #from flask import url_for, abort, flash
 import game as gamelib
@@ -21,22 +20,62 @@ import game
 
 #Briefly, we're creating our Flask application and telling flask where
 # it is in correlation to the file system and the python package
-app = Flask(__name__)
+# Per Flask documentation, it's better to use the package name then the
+# fully qualified path.  In this case __name__ is `py_rpg.app`
+# when we want just py_rpg.
+app = Flask(__name__.split('.')[0])
 
+#This tells flask that the web application itself is configured in this file
 app.config.from_object(__name__)
+#TODO - This needs to be changed but its not a super priority now as this
+# is how you secure cookies from abuse by third parties and respect
+# user privacy.
 app.secret_key = 'some_secret'
+
+#Not good behavior but we want to bond the gamelib module to the app
+# Flask instance so it can be retrieved later.
 app.gamelib = gamelib
 
 
 
 # request handlers -- these two hooks are provided by flask and we will use them
 # to create and tear down our game object.  NOTE until it's save'd the game is
-# emphemeral or will go away.
+# emphemeral or will go away each time it's reloaded.
 @app.before_request
 def before_request():
+    """
+        Before the py_rpg.views is even accessed, this is
+        called.
+
+        It should be quick and memory/cpu cheap.  Adding a lot of extra
+        logic here will slow the entire application down and increase
+        memory use for every single HTTP request.
+    """
     g.game = game.GameState.Create_or_rebuild(session)
+
 
 @app.after_request
 def after_request(response):
+    """
+        After views are done doing their work, this is called.
+
+        It's called a post-filter in some MVC architecture.  One of its uses is
+        to check output destined for the browser ( ie running html tidy, scrubbing for
+        specific words, or adding extra lines of code).  I, DevDave, frown on that
+        last bit as it gets confusing and or hard to debug.
+
+        In our case it allows us to do housekeeping, clean up work.  In this case
+        it saves
+    """
     g.game.save(session)
     return response
+    #In the future.
+    # Wrapping the game save logic in a try...finally ENSURES
+    # the application doesn't crash and burn.
+    # downside is that if there is something seriously wrong
+    # it will take longer to figure out what is it.
+    # a better approach would be to add an except clause and log the error.
+    #try:
+    #    g.game.save(session)
+    #finally:
+    #
--- py_rpg/game.py
+++ py_rpg/game.py
@@ -13,7 +13,8 @@
 rooms = {
     "room1": {
         "title": "Room2",
-        "connections": dict(east = "room2", south="room3")
+        "connections": dict(east = "room2", south="room3"),
+        "start_in": True #tell map where the entrance is!
     },
     "room2": {
         "title": "Room2",
@@ -28,6 +29,169 @@ rooms = {
         "connections": dict(west = "room3", north="room2")
     },
 }
+
+class Room(object):
+
+    def __init__(self, parent_map, room_id, **kwargs):
+        #Bad code idea, but it's brutally quick
+        self.parent = parent_map
+        self.my_id = room_id
+        self.start_in = False
+
+        assert hasattr(parent_map, "get_room")
+
+        for k, value in kwargs.items():
+            setattr(self, k, value)
+
+        #Ensure we have the minimum's
+        assert hasattr(self, "title")
+        assert hasattr(self, "connections")
+
+    #We use property decorators to protect
+    #the room data.  We don't want to ever modify these
+    #but instead have something like a creatures list like GameMap
+    #and a entities/property list like GameStuff or GameLoot.
+
+    @property
+    def id(self):
+        return self.my_id
+
+    @property
+    def whoami(self):
+        return self.id
+
+    @property
+    def map(self):
+        return self.parent_map
+
+    def describe(self):
+        """
+            returns [description, directions] with text markup
+
+            A list used to make it easier to do additional formatting later
+                (eg add <br> or \n or something along those lines)
+        """
+
+        room_text = "You are in {}".format(self.title)
+
+        directions_names = ", ".join(self.connections.keys())
+
+        direction_list += """You can go: {}""".format(directions_names)
+
+        return [room_text, direction_list]
+
+
+    @property
+    def directions(self):
+        return self.connections.items()
+
+    @property
+    def describe(self):
+        if hasattr(self, "description"):
+            return """You are in {}
+
+                You see {}
+                """.format(self.title, self.description)
+        else:
+            return """
+                You are in {}
+
+                It is a completely empty place with nothing remarkable
+                """
+
+    def go(self, direction, game = None):
+        """
+
+
+            The room should be able to dictage if you can leave it.
+
+            Say you're fighting an ExcessiveAbstractionMonster and you lose
+            the role
+        """
+        #Need to replace this with a raise WrongDirection except
+        assert direction in self.connections
+
+        return self.parent.get(self.connections[direction])
+
+
+
+class GameMap(object):
+    def __init__(self, game_map):
+        """
+
+            room_id:
+                title: string - require
+                connections: map/dict
+                    direction: room_id
+
+            Not implemented yet keys
+
+            room_id:
+                description
+                creatures: map of creatures
+                    creature_id: spawn_chance (0-1 eg 50% would be .5)
+
+        """
+        self.raw_map = game_map
+        self.rooms = {}
+        self.starting_id = None
+
+        self.validate_and_build()
+        assert self.starting_id
+
+    def validate_and_build(self):
+
+        for room_id, raw_room in self.raw_map.items():
+            new_room = Room(self, room_id, **raw_room)
+
+            if new_room.start_in == True:
+                self.starting_id = new_room.id
+
+            self.rooms[room_id] = new_room
+
+
+    def get(self, room_id):
+        assert room_id in self.rooms
+        return self.rooms[self.room_id]
+
+class Player(object):
+
+    def __init__(self, my_map, **kwargs):
+        self.map = my_map
+        self.my_location = None
+
+        if 'location' in kwargs:
+            self.my_location = location
+        else:
+            self.my_location = my_map.starting_room_id
+
+
+    @classmethod
+    def Create_or_rebuild(cls, session, my_map):
+
+        if 'player' in session:
+            return cls(my_map, **session['player'])
+        else:
+            return cls(my_map)
+
+
+
+    def move(self, direction):
+        new_room = my_map.get(self.my_location, direction)
+        self.my_location = new_room.id
+
+    def
+
+
+
+def initialize_game(self, source_map_filename = None):
+    """
+        TODO use source_map_filename to load the map from a file!
+    """
+    #We don't need to use a global as we're not modifying the
+    #global rooms variable.
+    return GameMap(rooms)
+
 
 
 class RoomHelper(object):
@@ -77,69 +241,56 @@ class RoomHelper(object):
 
 RH = RoomHelper
 
-class GameState(object):
-    """
-        Provides a wrapper around a game session.
-
-        This ideally makes it easier to add fundamental state information
-        in a semi-flexible way.  Downside is that this is a glorified singleton
-        which can get corrupted if not used carefully.
-    """
-    def __init__(self, session, new_game = None):
-
-        if not new_game:
-            #Ensure we have a location
-            if 'location' not in session['game']:
-                raise Exception("Uh oh, someone we lost our location!")
-
-            self.state = {k:v for k,v in session['game'].items()}
-
-        else: #it's a new GAME!
-            self.state = {}
-            self.state['new_game'] = True
-            #Bad code smell, constant like this should be more configurable
-            self.state['location'] = 'room1'
-            #Depend on the game to set our initial location!
-
-
-
-    @classmethod
-    def Create_or_rebuild(cls, session):
-        if 'game' not in session:
-            return cls(session, new_game = True)
-        else:
-            return cls(session, new_game = False)
-
-    def save(self, session_obj):
-        """
-            Save our game state back to flask's session object
-
-            Ensure the new_game flag is not put into the session when saving.
-        """
-
-        if 'game' not in session_obj:
-            session_obj['game'] = {}
-
-        for key, val in self.state.items():
-            #Skip new_game setting when saving
-            if key == 'new_game': continue
-            session_obj['game'][key] = val
-
-    @property
-    def location(self):
-        return self.state['location'] if 'location' in self.state else 'uh-oh'
-
-    @location.setter
-    def location(self, value):
-        self.state['location'] = value
-
-    @property
-    def is_new(self):
-        return 'new_game' in self.state
-
-
-
-
-
-
-
+#class GameState(object):
+#    """
+#        Provides a wrapper around a game session.
+#
+#        This ideally makes it easier to add fundamental state information
+#        in a semi-flexible way.  Downside is that this is a glorified singleton
+#        which can get corrupted if not used carefully.
+#    """
+#    def __init__(self, session, new_game = None, map_file = None):
+#
+#        self.state = {}
+#        self.new_game = new_game or False
+#        self.player = None
+#
+#        self.player = Player.Create_or_rebuild(session, self.state['map'] )
+#
+#
+#
+#
+#
+#    @classmethod
+#    def Create_or_rebuild(cls, session, starting_map_filename = None):
+#        if 'game' not in session:
+#            return cls(session, new_game = True, map_file = starting_map_filename)
+#        else:
+#            return cls(session, new_game = False)
+#
+#    def save(self, session_obj):
+#        """
+#            Save our game state back to flask's session object
+#
+#            Ensure the new_game flag is not put into the session when saving.
+#        """
+#
+#        if 'game' not in session_obj:
+#            session_obj['game'] = {}
+#
+#        for key, val in self.state.items():
+#            #Skip new_game setting when saving
+#            if key == 'new_game': continue
+#            session_obj['game'][key] = val
+#
+#    @property
+#    def location(self):
+#        return self.state['location'] if 'location' in self.state else 'uh-oh'
+#
+#    @location.setter
+#    def location(self, value):
+#        self.state['location'] = value
+#
+#    @property
+#    def is_new(self):
+#        return 'new_game' in self.state
