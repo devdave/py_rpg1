5f8b16bf24ec59014895abca1b055d8914bceb9f
David W <devdave@ominian.net>
2014-11-15 02:15:58 -0700

    Built out tests and iterated on GameState object

--- py_rpg/game/gamestate.py
+++ py_rpg/game/gamestate.py
@@ -1,6 +1,5 @@
 
-from player import Player
-from dungeon import Dungeon
+
 
 """
     Game state is created when the flask Application
@@ -9,4 +8,51 @@ from dungeon import Dungeon
 
 
 class GameState(object):
-    pass
+
+    def __init__(self):
+        self.dungeon = None
+        self.player_cls = None
+
+        self.player = None
+
+
+    def set_dungeon(self, dungeon):
+        if self.dungeon is not None:
+            raise ValueError("Dungeon has already been set, it cannot be assigned to again!")
+        self.dungeon = dungeon
+
+    def set_player_class(self, player_cls):
+        if self.player_cls is not None:
+            raise ValueError("The Player class has already been set, it cannot be assigned to again!")
+        self.player_cls = player_cls
+
+
+    def restore(self, session):
+        if self.player is not None:
+            raise ValueError("Gamestate has already been restored for this request!")
+
+        self.player = self.player_cls.Create_Rebuild(session)
+        self.player.current_dungeon = self.dungeon
+
+
+    def save(self, session):
+        self.player.save(session)
+
+        #TODO do whatever we need to ensure all of the
+        # session/request specific stuff is gone
+        self.player = None
+
+    def move_player(self, direction):
+        """
+            Here's where we could BLOCK movement
+
+            Does the player have a required item or
+            Is there a hostile creature and they failed escape?
+            or does the game just not want them to move?
+        """
+
+
+        new_room = self.player.current_room.get(direction)
+        self.player.location_id = new_room.id
+        return new_room
+
--- py_rpg/game/tests/test_gamestate.py
+++ py_rpg/game/tests/test_gamestate.py
@@ -1,10 +1,128 @@
 
+from nose.tools import eq_
+from nose.tools import raises
+
+
+from contextlib import contextmanager
+
+from py_rpg.game.gamestate import GameState
+from py_rpg.game.dungeon import Dungeon
+from py_rpg.game.player import Player
+
 #shortcut aliases
 import os
 ABP = os.path.abspath
 J = os.path.join
 DRN = os.path.dirname
 
+MAP_FILE = ABP(J(DRN(__file__), "map_data.json"))
+
+gamestate = None
+
+@contextmanager
+def my_app():
+    """
+        Simulates how py_rpg.app behaves
+
+    """
+    global gamestate
+    gamestate = GameState()
+    dungeon = Dungeon(MAP_FILE)
+    gamestate.set_dungeon(dungeon)
+
+    gamestate.set_player_class(Player)
+    yield gamestate
+    gamestate = None
+
 
 def test_initialization():
-    pass
+    my_state = GameState()
+    dungeon = Dungeon(MAP_FILE)
+    my_state.set_dungeon(dungeon)
+
+
+@raises(ValueError)
+def test_set_dungeon_breaks():
+    my_state = GameState()
+    dungeon = Dungeon(MAP_FILE)
+    my_state.set_dungeon(dungeon)
+    my_state.set_dungeon(dungeon)
+
+@raises(ValueError)
+def test_set_player_class__breaks():
+    my_state = GameState()
+    my_state.set_player_class(Player)
+    my_state.set_player_class(Player)
+
+@raises(ValueError)
+def test_restore_breaks():
+    session = {}
+    with my_app() as gamestate:
+        gamestate.restore(session)
+        gamestate.restore(session)
+
+def test_restore_AND_save():
+    session = {
+        "player": {
+            "location_id":"room_1"
+        }
+    }
+    new_session = {}
+    with my_app() as gamestate:
+
+        gamestate.restore(session)
+        assert gamestate.player is not None
+        #we should be in room_1
+        eq_(gamestate.player.location, "room_1")
+
+        #Move the player to room_2 now
+        gamestate.player.location = "room_2"
+
+        gamestate.save(new_session)
+
+
+    assert new_session.get("player", False)
+    player_obj = new_session['player']
+
+    eq_(player_obj['location_id'], "room_2")
+
+
+    with my_app() as gamestate:
+        gamestate.restore(new_session)
+        assert gamestate.player is not None
+        eq_(gamestate.player.location, "room_2")
+
+
+def test_gamestate_move_player():
+    session = {}
+    on_save_session = {}
+    with my_app() as gamestate:
+        gamestate.restore(session)
+        eq_(gamestate.player.location, "room_1")
+
+        gamestate.move_player("east")
+        eq_(gamestate.player.location, "room_2")
+
+        gamestate.save(on_save_session)
+
+
+    eq_(on_save_session['player']['location_id'], "room_2")
+    print str(on_save_session)
+
+def test_player_current_dungeon():
+
+    session = {}
+    with my_app() as gamestate:
+        gamestate.restore(session)
+        my_dungeon = gamestate.dungeon
+        my_player = gamestate.player
+        eq_(my_dungeon, my_player.current_dungeon)
+
+def test_player_current_dungeon():
+
+    session = {}
+    with my_app() as gamestate:
+        gamestate.restore(session)
+        my_dungeon = gamestate.dungeon
+        my_player = gamestate.player
+        eq_(my_dungeon, my_player.current_dungeon)
