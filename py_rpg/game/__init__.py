502d61d96a26755dcb1496aae787750e3de485b0
David W <devdave@ominian.net>
2014-11-15 02:18:43 -0700

    cleaned up game module init

--- py_rpg/game/__init__.py
+++ py_rpg/game/__init__.py
@@ -1,6 +1,4 @@
 
 import gamestate
-#Problematic as this tightly couples these two files to Gamestate
-# but GameState REQUIRES them so maybe not that bad
-Player = gamestate.Player
-Dungeon = gamestate.Dungeon
+from player import Player
+from dungeon import Dungeon
---
+++ py_rpg/game/tests/test_integration.py
@@ -0,0 +1,8 @@
+#shortcut aliases
+import os
+ABP = os.path.abspath
+J = os.path.join
+DRN = os.path.dirname
+
+#Common data
+MAP_FILE = ABP(J(DRN(__file__), "map_data.json"))
