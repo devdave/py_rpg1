37963b73132a294dc645e66b736e21b8bafebf6d
David W <devdave@ominian.net>
2014-11-15 02:15:18 -0700

    Cleanup, convert connections values to Room object refs, implement get method

--- py_rpg/game/dungeon.py
+++ py_rpg/game/dungeon.py
@@ -5,8 +5,12 @@ class Room(object):
     def __init__(self, my_id, my_data):
         self.id = my_id
         self.title = my_data.get("title", my_id.capitalize())
-        self.connections = my_data.get("connections", {})
+        self.connections = my_data.get("connections")
+
         assert len(self.connections) > 0, "Room has no way out!"
+
+    def get(self, direction):
+        return self.connections[direction]
 
 
 class Dungeon(object):
@@ -24,6 +28,16 @@ class Dungeon(object):
 
             if data.get('start_in', False) != False:
                 self.starting_location = room_id
+
+
+
+        #Now fix up connections
+        for room_id, room in self.rooms.items():
+            new_connections = {}
+            for direction, direction_room_id in room.connections.items():
+                new_connections[direction] = self.rooms[direction_room_id]
+
+            room.connections = new_connections
 
     def get(self, room_id):
         return self.rooms.get(room_id)
