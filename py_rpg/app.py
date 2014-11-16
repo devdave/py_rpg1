fc9de7361b1d36906c0143fa458342a5a306b1b0
David W <devdave@ominian.net>
2014-11-15 17:40:14 -0700

    Cleanup and integrate our game package

--- py_rpg/app.py
+++ py_rpg/app.py
@@ -2,22 +2,22 @@
     The flask app object/module for the project.
 
 """
+#Flask dependancy imports
 from flask import Flask
-
 #flask.g is a global data proxy, it's a way for objects to
 # jump the train tracks and allow for separate parts of the application
 # to share common data.  In this case, g is where the game state object is
 # being assigned to make it available to the rest of the app.
 from flask import g
-
 from flask import session
-#from flask import url_for, abort, flash
-import game as gamelib
-#TODO change game.py over to gamelib to make it easier to read later
-# ie "Is it game the module, game the class, or game the instance?"
-import game
 
+#Our utility helpers
+from lib import HERE
 
+#Game logic imports
+from game import (GameState, Player, Dungeon,)
+
+#Setup our web application
 #Briefly, we're creating our Flask application and telling flask where
 # it is in correlation to the file system and the python package
 # Per Flask documentation, it's better to use the package name then the
@@ -27,14 +27,16 @@ app = Flask(__name__.split('.')[0])
 
 #This tells flask that the web application itself is configured in this file
 app.config.from_object(__name__)
-#TODO - This needs to be changed but its not a super priority now as this
-# is how you secure cookies from abuse by third parties and respect
+#TODO - This needs to be changed but its not a super priority now.  This
+# is how you secure cookies from abuse by third parties and protect
 # user privacy.
 app.secret_key = 'some_secret'
 
-#Not good behavior but we want to bond the gamelib module to the app
-# Flask instance so it can be retrieved later.
-app.gamelib = gamelib
+#This is our game, we setup our Game, add a dungeon, and tell it how to create
+# new players.
+app.game = GameState()
+app.game.set_dungeon(HERE(__file__, "data/basic_map.json"))
+app.game.set_player_class(Player)
 
 
 
@@ -51,7 +53,7 @@ def before_request():
         logic here will slow the entire application down and increase
         memory use for every single HTTP request.
     """
-    g.game = game.GameState.Create_or_rebuild(session)
+    app.game.restore(session)
 
 
 @app.after_request
@@ -67,15 +69,17 @@ def after_request(response):
         In our case it allows us to do housekeeping, clean up work.  In this case
         it saves
     """
-    g.game.save(session)
+    app.game.save(session)
     return response
     #In the future.
     # Wrapping the game save logic in a try...finally ENSURES
     # the application doesn't crash and burn.
     # downside is that if there is something seriously wrong
-    # it will take longer to figure out what is it.
+    # it will take longer to debug what has gone wrong.
     # a better approach would be to add an except clause and log the error.
     #try:
     #    g.game.save(session)
+    #except Exception as e:
+    #    log.error(e)
     #finally:
     #
