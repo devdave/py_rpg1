a2cd61d51655ccb40650c52276e8e398aad4fa1e
David W <devdave@ominian.net>
2014-11-15 18:57:14 -0700

    clean up watch_tests script

--- watch_tests.bat
+++ watch_tests.bat
@@ -4,4 +4,4 @@ set test_script=%1
 watchmedo shell-command ^
     --patterns="*.py;*.json" ^
     --recursive ^
-    --command "nosetests --with-coverage --cover-tests   --cover-html --cover-branches --cover-package=py_rpg"
+    --command %test_script%
