a6bd204edd797544ab4596faecbfabc148d85815
David W <devdave@ominian.net>
2014-11-16 04:56:36 -0700

    Temporarily watch our map file and reload
    This should speed up some dev time as the base_map is now considered
    part of the application by Flask

--- py_rpg/main.py
+++ py_rpg/main.py
@@ -1,8 +1,12 @@
 from app import app
 import views
 
+from lib import HERE
 
 if __name__ == '__main__':
 
     app.debug = True
-    app.run(host="0.0.0.0")
+
+    app.run(host="0.0.0.0",
+        extra_files=[HERE(__file__, "maps/base_map.json")]
+    )
