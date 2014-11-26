b2e22326fa3d2ea04c149de35981d81e191f6022
David W <devdave@ominian.net>
2014-11-25 19:01:28 -0700

    WS reformating

--- prototypes/ping_js/libs/ping.input.js
+++ prototypes/ping_js/libs/ping.input.js
@@ -13,28 +13,28 @@ the arrow keys to see if they were pressed or unpressed (up or down);
 @lends
 */
 ping.Lib.initInput = function(){
-            /**
-            @function
-            */
-            ping.Lib.Input = function(){
-                        var self = this;
-                        var map    = { 38: "U", 40: "D", 37: "L", 39: "R" };
-                        self.state = { U: false, D: false, L: false, R: false };
-                        self.keydown = function(evnt){
-                            var mKey = map[evnt.which];
-                            if(mKey != "undefined"){
-                                self.state[mKey] = true;
-                            }
-                        }
+    /**
+    @function
+    */
+    ping.Lib.Input = new function(){
+        var self = this;
+        var map    = { 38: "U", 40: "D", 37: "L", 39: "R" };
+        self.state = { U: false, D: false, L: false, R: false };
+        self.keydown = function(evnt){
+            var mKey = map[evnt.which];
+            if(mKey != "undefined"){
+                self.state[mKey] = true;
+            }
+        }
 
-                        self.keyup = function(evnt){
-                            var mKey = map[evnt.which];
-                            if(mKey){
-                                self.state[mKey] = false;
-                            }
-                        }
+        self.keyup = function(evnt){
+            var mKey = map[evnt.which];
+            if(mKey){
+                self.state[mKey] = false;
+            }
+        }
 
-                        $(document).keydown(self.keydown);
-                        $(document).keyup(self.keyup);
-                    }
+        $(document).keydown(self.keydown);
+        $(document).keyup(self.keyup);
+    }
 }
