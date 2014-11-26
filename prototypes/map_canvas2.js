1d946cfc99aacf8e2968bb04bb41c40abfe26af6
David W <devdave@ominian.net>
2014-11-25 19:08:25 -0700

    It's ugly as sin, but proof of concept for scroller logic

--- prototypes/map_canvas2.js
+++ prototypes/map_canvas2.js
@@ -1,201 +1,84 @@
-/**
-    Prototype 2 goals
 
-    Build a GameMap object that holds a list of
-    all rooms.
-
-    Setup 2 canvas tags
-*/
+GameTile = function(col, row, parent, fillStyle) {
+    /**
+    GameTile doesn't know the size of the canvas
+    it's going to be so instead it knows what position it
+    is on a grid.
 
 
+    */
+    this.x = col;
+    this.y = row;
 
-function renderBox(ctx, x, y, h, w, fill) {
-    "use strict";
+    this.sx = 1;
+    this.sy = 1;
+    this.fillStyle = fillStyle;
 
-    ctx.beginPath();
-
-    if (fill === true) {
-        ctx.fillRect(x, y, h, w);
-    } else {
-        ctx.strokeRect(x, y, h, w);
-    }
+    this.parent = parent;
 }
 
-function interconnectLines(ctx,
-        bX, //Top left corner
-        bY, //Top left corner
-        bW, // width
-        bH, //Height - TODO abstract box to an object
-        doHorz, //Optional
-        doVert  //Optional
-        ) {
-
+GameTile.prototype.render = function(ctx, scale_x, scale_y) {
     "use strict";
+    var x = this.x * scale_x,
+        y = this.y * scale_y;
 
-    if (doHorz === false && doVert === false) {
-        return;
-    }
-
-    //horizontal first
-    //we want to draw from the middle of the box to the next
-    //box over.  It is assumed that higher level
-    //logic above this ensure's there IS in fact box
-
-    //The target is the boxes's center
-    var halfW = bW / 2,
-        halfH = bH / 2,
-        boxRight = { x: bX + bW + halfW, y: bY + halfH  },
-        boxDown  = { x: bX + halfW, y: bY + bH + halfH },
-        boxCur   = { x: bX, y: bY };
-
-
-    function center() {
-        ctx.moveTo(boxCur.x + halfW, boxCur.y + halfH);
-    }
-    ctx.save();
-    ctx.beginPath();
-
-    if (doHorz) {
-        //It's just a step to the right!
-        center();
-        ctx.lineTo(boxRight.x, boxRight.y);
-    }
-
-    if (doVert) {
-        //And then a jump down!
-        center();
-        ctx.lineTo(boxDown.x, boxDown.y);
-
-    }
-
-    ctx.stroke();
-    ctx.restore();
-
-    return;
+    ctx.rect(x, y, scale_x, scale_y);
 }
 
-function initialize_map(map, gridColCnt, gridRowCnt) {
+
+GameMap = function(map_data) {
     "use strict";
-    var map_cnv = document.getElementById("canvas_map"),
-        map_ctx = map_cnv.getContext("2d"),
-        cWidth = map_cnv.width,
-        cHeight = map_cnv.height,
 
-        boxWidth = cWidth / gridColCnt,
-        boxHeight = cHeight / gridRowCnt,
+    this.width = map_data.cols;
+    this.height = map_data.rows;
+    this.grid = ping.QuadrantFactory(this.width, this.height);
+    this.elements = [];
 
-        squareInsetDepth = 0.30,
-        insetWidth = boxWidth * squareInsetDepth,
-        insetHeight = boxHeight * squareInsetDepth,
-        //squareMaxRowSize = cWidth / gridRowCnt,
-        //squareMaxColSize = cHeight / gridColCnt,
+    var colors = ["red","blue","green","yellow", "orange"],
+        row, col, myColor, debug;
 
+    for (row = 0; row < this.height; row++) {
+        alert("stop!");
+        for(col = 0; col < this.width; col++){
+            alert("stop!");
+            myColor = colors[Math.floor(Math.random() * colors.length)];
+            var tile = new GameTile(col, row, parent, myColor );
+            this.grid.add(tile);
+            this.elements.push(tile);
+        }
+    }
+    debug = this.grid.getAll();
+    console.log(this.height * this.width, debug.length, debug.length == this.height * this.width);
+}
 
-        //squareInsetX = boxWidth * (squareInsetDepth * 2),
-        //squareInsetY = boxHeight * squareInsetDepth,
-        //squareInsetH = boxHeight - (squareInsetX * 2),
-        //squareInsetW = boxWidth - squareInsetY * 2,
-        showPerimeter = true,
-        showBox = true,
-        showLine = true,
-        xPos = 0,
-        yPos = 0;
-        //halfX = 0,
-        //halfY = 0,
-        //nextX = 0,
-        //nextY = 0;
+GameMap.prototype.render = function(ctx, offsetx, offsety, limit_x, limit_y) {
+    "use strict";
+    var scale_x = ctx.canvas.width / this.width,
+        scale_y = ctx.canvas.height / this.height,
+        offsetx = offsetx || 0,
+        offsety = offsety || 0,
+        limit_x = limit_x || this.width,
+        limit_y = limit_y || this.height,
+        search_box = {x:offsetx, y:offsety, sx:limit_x, sy: limit_y},
+        my_elements, debug_element;
 
-    //Make all lines crisper
-    map_ctx.translate(0.5, 0.5);
-
-    map_ctx.strokeStyle = "black";
-    map_ctx.save();
-    map_ctx.beginPath();
-
-
-
-    for (var r = 0; r < gridColCnt; r++) {
-        //Been a while since I've worked with c++ ;)
-
-        for(var c = 0; c < gridRowCnt; c++) {
-
-            xPos = c*boxWidth;
-            yPos = r*boxHeight;
-
-
-            map_ctx.moveTo(xPos,yPos);
-
-            if (showPerimeter) {
-
-                map_ctx.save();
-                map_ctx.strokeStyle = "black";
-                renderBox(map_ctx, xPos, yPos, boxWidth, boxHeight );
-
-                map_ctx.restore();
-            }
-
-            if (showLine) {
-
-                map_ctx.strokeStyle = "orange";
-                interconnectLines(
-                    map_ctx,
-                    xPos,
-                    yPos,
-                    boxWidth,
-                    boxHeight,
-                    c+1 < gridRowCnt,
-                    r+1 < gridColCnt
-                );
-
-            }
-
-
-            if (showBox) {
-                map_ctx.save();
-                map_ctx.fillStyle = "blue";
-                /**
-                    I wrote this and it still confused
-                    me when I pass over it.
-
-                    So we want the interior square to have
-                    10% padding around it.
-
-                    So 1st two arguments, we move our box
-                    down and right by 10%... BUT the box
-                    itself is still 100% sized.
-
-                    We still want 10% padding per side.
-                    left + right + box.width must equal 100
-
-                    the 2 & 3rd argument + inset H & W
-                    only pushes the box 10% down and 10% right
-                    but the box is still 100%.
-
-                    So for arguments 4 & 5, you shrink the
-                    box by 20% so that you get 10% padding
-                    on all sides.
-
-                */
-                renderBox(map_ctx,
-                           xPos + insetWidth,
-                           yPos + insetHeight,
-                           boxWidth - ( insetWidth * 2),
-                           boxHeight - ( insetHeight * 2) ,
-                           true
-                );
-                map_ctx.restore();
-
-
-            }
-
-
+        if (arguments.length == 1) {
+            my_elements = this.elements;
+        } else {
+            my_elements = this.grid.findBox(search_box);
+            debug_element = this.grid.getAll();
+            console.log("found", my_elements.length);
         }
 
 
+
+    for( var i = 0; i < my_elements.length; i++) {
+        ctx.beginPath();
+        my_elements[i].render(ctx, scale_x, scale_y);
+        ctx.fillStyle = my_elements[i].fillStyle;
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath;
     }
-    //map_ctx.stroke();
-    map_ctx.restore();
 
-
-
-};
+}
