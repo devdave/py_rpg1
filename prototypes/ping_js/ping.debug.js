b0036e8dcf9c4922aac222990f11db5f53aa834a
David W <devdave@ominian.net>
2014-11-27 00:26:01 -0700

    Update prod/dev files

--- prototypes/ping_js/ping.debug.js
+++ prototypes/ping_js/ping.debug.js
@@ -1090,3 +1090,43 @@ ping.Lib.Quadrant.prototype.findBox = function(box){
 ping.QuadrantFactory = function(width, height, max) {
     return new ping.Lib.Quadrant(0,0, width, height, max || 4 );
 }
+
+ping.Math = ping.Math || {};
+
+
+/**
+
+width = 3
+height = 3
+
+0 1 2
+3 4 5
+6 7 8
+
+[0,0] [1,0] [2,0]
+[0,1] [1,1] [2,1]
+[0,2] [1,2] [2,3]
+
+0 1 2 3 4 5 6 7 8
+
+given - [1,2]
+(x1 * w3)+ y2  == 7
+
+given - 7
+$y = Math.floor(7/3) == 2
+$x = 7 - ($y*3) == 1
+
+*/
+ping.Math.c2i = function(x,y, sw, sh) {
+    return (x * sw) + y;
+}
+
+ping.Math.i2c = function(i, sw, sh) {
+    var x = Math.floor(i/sw),
+        y = i - (x*sh);
+    return [x, y];
+}
+
+//function(x, y, width, height) {
+//
+//}
